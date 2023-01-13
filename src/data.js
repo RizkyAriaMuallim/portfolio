301636b61db888523a2ca5685c5b6819ce274ccb
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-01-13 20:15:18 +0700

    push portfolio-site

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "Simple Calculator",
+    subtitle: "HTML, CSS, JavaScript",
+    description:
+      "This is just a simple calculator and have fiture that can show your calculate-history",
+    image: "./project-1.gif",
+    link: "https://github.com/RizkyAriaMuallim/simple-web-calculator",
+  },
+  {
+    title: "Static Web about My Collection",
+    subtitle: "HTML, CSS, JavaScript",
+    description:
+      "This is simple static one-page that will show my tomica collection",
+    image: "./project-2.gif",
+    link: "https://github.com/RizkyAriaMuallim/webAboutMyCollections",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "JavaScript",
+  "React",
+  "Tailwind"
+];
